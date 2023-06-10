f94dd0cdcf77689a4c9b0db7877d6d869967d13e
DebaratiD <debaratidatta.77star@gmail.com>
2023-06-10 00:56:45 -0700

    first commit

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try{
+    const r = await axios.put("https://api.chatengine.io/users/",
+    {"username": username, "secret": username, "first_name":username},
+    {headers:{"private-key":"f9a4a850-b1ef-4f8e-b8ca-b13abfa619f0"}});
+    return res.status(r.status).json(r.data);
+  }
+  catch(err){
+    return res.status(err.response.status).json(err.response.data);
+  }
+
+});
+
+app.listen(3001);//use port 3001
