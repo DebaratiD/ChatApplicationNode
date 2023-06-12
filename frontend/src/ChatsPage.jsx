0407ba7ba11a9ef211a9d5da6d1efedf1cdb22c1
DebaratiD <debaratidatta.77star@gmail.com>
2023-06-12 00:18:30 -0700

    chat-engine-pretty version

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+//import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
+import { PrettyChatWindow } from 'react-chat-engine-pretty';
 
 const ChatsPage = (props) =>{
-  const chatprops = useMultiChatLogic(
-    'd1f467cb-6d29-4fcd-8576-3e7e9ad02a71', 
-    props.user.username, props.user.secret);
+  // const chatprops = useMultiChatLogic(
+  //   'd1f467cb-6d29-4fcd-8576-3e7e9ad02a71', 
+  //   props.user.username, props.user.secret);
   return (
     <div style={{height:'100vh'}}>
-      <MultiChatSocket {...chatprops}/>
-      <MultiChatWindow {...chatprops} style={{height: '100%'}}/>
+      <PrettyChatWindow 
+      projectId='d1f467cb-6d29-4fcd-8576-3e7e9ad02a71'
+      username={props.user.username} 
+      secret={props.user.secret}
+      style={{height: '100%'}}
+      />
     </div>
   )
 }
