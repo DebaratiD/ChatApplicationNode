466b5d698e4b8afe987fd9ee24a3782b55299579
DebaratiD <debaratidatta.77star@gmail.com>
2023-06-11 23:44:55 -0700

    App using react-chat-engine-advance complete

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced';
 
-export default function ChatsPage() {
+const ChatsPage = (props) =>{
+  const chatprops = useMultiChatLogic(
+    'd1f467cb-6d29-4fcd-8576-3e7e9ad02a71', 
+    props.user.username, props.user.secret);
   return (
-    <div>Chats...</div>
+    <div style={{height:'100vh'}}>
+      <MultiChatSocket {...chatprops}/>
+      <MultiChatWindow {...chatprops} style={{height: '100%'}}/>
+    </div>
   )
 }
+export default ChatsPage;
